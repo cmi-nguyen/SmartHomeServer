15ed2c95662a46dfa1b0ea4c722fe05a6f162664
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2024-10-29 22:42:25 +0700

    - Clear unessesary code

--- src/main/java/com/example/demo/repositories/BillDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.BillDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillDetailRepository extends JpaRepository<BillDetail,Integer> {
-}
--- src/main/java/com/example/demo/repositories/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill,Integer> {
-}
