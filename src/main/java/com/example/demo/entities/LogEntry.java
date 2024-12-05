82b833fe42dff4b311d35da8a6b4746f84eb37e3
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2024-12-05 21:40:36 +0700

    Update Logentry

--- src/main/java/com/example/demo/entities/LogEntry.java
+++ src/main/java/com/example/demo/entities/LogEntry.java
@@ -12,14 +12,44 @@ public class LogEntry {
     @Column(name = "log_id")
     @JdbcTypeCode(SqlTypes.INTEGER)
     private Integer logID;
+
+    public Integer getLogID() {
+        return logID;
+    }
+
+    public void setLogID(Integer logID) {
+        this.logID = logID;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     @Column(name="user")
-//    @Convert(converter =  AESEncryption.class)
     private  String user;
     @Column(name = "action")
-//    @Convert(converter =  AESEncryption.class)
     private String action;
     @Column(name = "date")
-//    @Convert(converter =  AESEncryption.class)
     private String date;
 
 }
